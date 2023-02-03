dcd1bc813f1a311cf519e38f103eac875185978a
maverickdevop <57834199+maverickdevop@users.noreply.github.com>
2023-02-03 14:41:56 +0300

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,9 +1,11 @@
+""" This is a class with test data """
+
 class TestData:
 
-    """ Website Sources """
+    """ Website main URL """
     BASE_URL = "https://www.adidas.fi/"
 
-    """ Search Data """
+    """ Search data parameters """
     SEARCH_WORD = "Adidas ZX 2K"
     SEARCH_TITLE = "adidas Official Website FI"
     SEARCH_ASSERT = "ZX 2K"
