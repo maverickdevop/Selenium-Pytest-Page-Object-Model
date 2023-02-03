9419e9730e83b8cd572218e8acab4a93e3d63d7c
maverickdevop <57834199+maverickdevop@users.noreply.github.com>
2023-02-03 14:42:34 +0300

    Update global_enums.py

--- Enums/global_enums.py
+++ Enums/global_enums.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 
+""" This is a class of global error messages """
+
 class GlobalErrorMessages(Enum):
     WRONG_TITLE = "incorrect page title!"
     WRONG_URL = "Incorrect URL page source!"
